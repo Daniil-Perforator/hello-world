eef9fe916145bd193e64ec513d22f9d7fee9963a
Daniil perforator <gareb.show@gmail.com>
2021-06-06 19:50:07 +0400

    фывфыв

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "hello";
+        String world = "world";
+        System.out.println(hello+" "+world+"!");
+    }
+}
